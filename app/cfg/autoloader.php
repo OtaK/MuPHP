cf67f25aa5c9c46962471bc392cc27e0969d79ca
Mathieu Amiot <otak_@otak-arts.com>
2012-07-27 09:26:30 +0200

    Fixed autoloader with svn folders

--- app/cfg/autoloader.php
+++ app/cfg/autoloader.php
@@ -5,11 +5,11 @@
         $libFolders = scandir(__DIR__.'/../lib/');
         foreach ($libFolders as $folder)
         {
-            if ($folder == '.' || $folder == '..' || $folder == 'tests') continue;
+            if ($folder == '.' || $folder == '..' || $folder == 'tests' || $folder == '.svn') continue;
             $libFiles = scandir(__DIR__.'/../lib/'.$folder);
             foreach ($libFiles as $file)
             {
-                if ($file == '.' || $file == '..' || $file == 'server.php') continue;
+                if ($file == '.' || $file == '..' || $file == 'server.php' || $file == 'includes') continue;
                 include_once __DIR__.'/../lib/'.$folder.'/'.$file;
             }
         }
