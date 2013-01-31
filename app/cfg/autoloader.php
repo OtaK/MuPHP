ac00617e9209f8e3be6c6a8c3ff26b1c740b68ee
Mathieu Amiot <otak_@otak-arts.com>
2013-01-31 12:47:08 +0100

    Autoloader optimization

--- app/cfg/autoloader.php
+++ app/cfg/autoloader.php
@@ -5,12 +5,12 @@
         $libFolders = scandir(__DIR__.'/../lib/');
         foreach ($libFolders as $folder)
         {
-            if ($folder == '.' || $folder == '..' || $folder == 'tests' || $folder == '.svn') continue;
-            $libFiles = scandir(__DIR__.'/../lib/'.$folder);
+            if ($folder == '.' || $folder == '..' || $folder == 'tests') continue;
+            $libFiles = scandir(__DIR__."/../lib/{$folder}");
             foreach ($libFiles as $file)
             {
-                if ($file == '.' || $file == '..' || $file == 'api.php' || $file == 'includes') continue;
-                include_once __DIR__.'/../lib/'.$folder.'/'.$file;
+                if ($file == '.' || $file == '..') continue;
+                include_once __DIR__."/../lib/{$folder}/{$file}";
             }
         }
     }
