28a20f16b55dc1c55e56b4d455ffcf31bdba9af1
Mathieu Amiot <m.amiot@otak-arts.com>
2013-03-17 18:01:15 +0100

    Updated cache provider to 1.0

--- app/lib/cache/CacheProvider.php
+++ app/lib/cache/CacheProvider.php
@@ -1,5 +1,24 @@
 <?php
+
+    /*
+     * Copyright 2013 Mathieu "OtaK_" Amiot <m.amiot@otak-arts.com> http://mathieu-amiot.fr/
+     *
+     * Licensed under the Apache License, Version 2.0 (the "License");
+     * you may not use this file except in compliance with the License.
+     * You may obtain a copy of the License at
+     *
+     *      http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Unless required by applicable law or agreed to in writing, software
+     * distributed under the License is distributed on an "AS IS" BASIS,
+     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     * See the License for the specific language governing permissions and
+     * limitations under the License.
+     *
+     */
+
     namespace MuPHP\Cache;
+    require_once __DIR__ . '/../abstraction/designPatterns.php';
 
     class MemcacheNotFoundException extends \Exception
     {
@@ -25,6 +44,16 @@
         }
     }
 
+    /**
+     * Caching engine with internal json serialization
+     * @package    MuPHP
+     * @subpackage Cache
+     * @author     Mathieu AMIOT <m.amiot@otak-arts.com>
+     * @copyright  Copyright (c) 2013, Mathieu AMIOT
+     * @version    1.0
+     * @changelog
+     *      1.0 : Initial release
+     */
     class CacheProvider extends \MuPHP\DesignPatterns\Singleton
     {
         const
@@ -41,6 +70,18 @@
          * @var \Redis|\Memcache
          */
         private $_provider;
+
+        /**
+         * Singleton stuff
+         * @param int $provider
+         * @return CacheProvider
+         */
+        public static function get_instance($provider = CACHE_DEFAULT_ENGINE)
+        {
+            if (!isset(self::$_instance))
+                self::$_instance = new CacheProvider($provider);
+            return self::$_instance;
+        }
 
         /**
          * Ctor
@@ -99,52 +140,76 @@
          * Set a key-value pair (or several) in caching provider
          * @param string|array $key
          * @param string|int|array $value
-         * @return bool
+         * @param float|int $timeout
          * @throws CacheEngineNotSet
+         * @return bool
          */
-        public function set($key, $value)
+        public function set($key, $value, $timeout = 0)
         {
             if ($this->_currentProvider === self::CACHE_NONE)
                 throw new CacheEngineNotSet();
 
             if (is_array($key) && is_array($value)) // If we have arrays of keys & values, handle it like a sir
-                return $this->multiSet($key, $value);
+                return $this->multiSet($key, $value, $timeout);
 
+            $cacheKey = $this->_prefixKey($key);
             $cacheValue = is_array($value) ? json_encode($value) : $value;
 
-            return $this->_provider->set($this->_prefixKey($key), $cacheValue);
+            if ($this->_currentProvider === self::CACHE_REDIS)
+                return $timeout ? $this->_provider->setex($cacheKey, (int)$timeout, $cacheValue) : $this->_provider->set($cacheKey, $cacheValue);
+            else if ($this->_currentProvider === self::CACHE_MEMCACHE)
+                return $this->_provider->set($cacheKey, $cacheValue, $timeout);
+
+            return false;
         }
 
         /**
          * Set multiple key-value pairs in caching provider
          * @param array $keys
          * @param array $values
+         * @param float|int $timeout
          * @return bool
          * @throws CacheEngineNotSet
          */
-        public function multiSet(array $keys, array $values)
+        public function multiSet(array $keys, array $values, $timeout = 0)
         {
             if ($this->_currentProvider === self::CACHE_NONE)
                 throw new CacheEngineNotSet();
 
             $l  = count($keys);
             if ($l !== count($values)) return false;
-            $result = true;
 
-            for ($i = 0; $i < $l; ++$i)
+            // Prefix all keys
+            $mKeys = array();
+            foreach ($keys as $k)
+                $mKeys[] = $this->_prefixKey($k);
+
+            // Encode values if necessary
+            foreach ($values as &$val)
+                $val = is_array($val) ? json_encode($val) : $val;
+
+            if ($this->_currentProvider === self::CACHE_REDIS)
             {
-                $cacheValue = is_array($values[$i]) ? json_encode($values[$i]) : $values[$i];
-                if (!$this->_provider->set($this->_prefixKey($keys[$i]), $cacheValue))
-                    $result = false;
+                return $this->_provider->msetnx(array_combine($mKeys, $values));
+            }
+            else if ($this->_currentProvider === self::CACHE_MEMCACHE)
+            {
+                $result = true;
+                for ($i = 0; $i < $l; ++$i)
+                {
+                    if (!$this->_provider->set($mKeys[$i], $values[$i], 0, $timeout))
+                        $result = false;
+                }
+                return $result;
             }
 
-            return $result;
+            return false;
         }
 
         /**
          * Get a value (or several) from its key
          * @param string|array $key
-         * @return array|string
+         * @return array|string|bool
          * @throws CacheEngineNotSet
          */
         public function get($key)
@@ -156,7 +221,13 @@
                 return $this->multiGet($key);
 
             $cacheKey = $this->_prefixKey($key);
-            return $this->_provider->get($cacheKey);
+            $result = $this->_provider->get($cacheKey);
+
+            $tmp = json_decode($result, true);
+            if (json_last_error() === JSON_ERROR_NONE)
+                return $tmp;
+
+            return $result;
         }
 
         /**
