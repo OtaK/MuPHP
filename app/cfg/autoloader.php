7a5c5277e98aaf4c901e1aa059777de79b20e9cb
Mathieu Amiot <m.amiot@otak-arts.com>
2013-03-21 15:09:02 +0100

    Fixed autoloader

--- app/cfg/autoloader.php
+++ app/cfg/autoloader.php
@@ -5,12 +5,23 @@
         $libFolders = scandir(__DIR__.'/../lib/');
         foreach ($libFolders as $folder)
         {
-            if ($folder == '.' || $folder == '..' || $folder == 'tests') continue;
+            if ($folder == '.' || $folder == '..' || $folder == 'tests' || $folder == '.svn') continue;
             $libFiles = scandir(__DIR__."/../lib/{$folder}");
             foreach ($libFiles as $file)
             {
-                if ($file == '.' || $file == '..') continue;
+                if ($file == '.' || $file == '..' || strpos($file, '.inc') !== false) continue;
                 include_once __DIR__."/../lib/{$folder}/{$file}";
             }
         }
     }
+
+  /*  function MuPHP_spl_autoloader($name)
+    {
+        $arch = explode('\\', $name);
+        $fileName = __DIR__.'/../lib/';
+        for ($i = 0, $l = count($arch); $i < $l; ++$i)
+        {
+            $fileName .=
+        }
+        if (file_exists())
+    }*/
